e8eed47b071c6802ca9cb613c62121c03bd5f6d3
ShuklaG1608 <46064636+ShuklaG1608@users.noreply.github.com>
2021-09-23 22:54:50 +0530

    Initial commit

---
+++ Basics/1.AdditionOfTwoNumber.py
@@ -0,0 +1,6 @@
+#Addition of Two Number
+
+num1 = int(input(" Enter First Number : "))
+num2 = int(input(" Enter Second Number : "))
+SUM = num1 + num2
+print(f' The Addition of {num1} and {num2} = {SUM}')
---
+++ Basics/2.MaxiumOfTwoNumber.py
@@ -0,0 +1,8 @@
+#Maximun of Two Number
+
+num1 = int(input(" Enter First Number : "))
+num2 = int(input(" Enter Second Number : "))
+if(num1>num2):
+    print(f' {num1} is greater than {num2}.')
+else:
+    print(f' {num2} is greater than {num1}.')
