ab070d048d766d29742fe25b05bf944373d4b7fd
ShuklaG1608 <46064636+ShuklaG1608@users.noreply.github.com>
2021-09-23 23:35:56 +0530

    Area Of Circle

---
+++ Basics/7.AreaOfCircle.py
@@ -0,0 +1,6 @@
+#Program To Find Area Of Circle
+
+radius = float(input(" Please Enter The Radius of The Circle : "))
+area = 3.1415 * (radius * radius)
+print(f' The Area of the Circle of the Radius {radius} = {area} ')
+
