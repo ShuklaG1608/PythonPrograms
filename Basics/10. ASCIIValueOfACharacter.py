8cb6cc6b29e676ae12d2d1072fa12f9830e44c35
ShuklaG1608 <46064636+ShuklaG1608@users.noreply.github.com>
2021-09-25 00:04:39 +0530

    ASCII Value of Given Character

---
+++ Basics/10. ASCIIValueOfACharacter.py
@@ -0,0 +1,7 @@
+#Program To Print ASCII Value of Givem Character
+
+char = input(" Please Enter A Single Character Whose ASCII Value is Required : ")[0]
+asciiVal =  ord(char)
+print(f' The ASCII Value of {char} = {asciiVal} ' )
+
+             
